8786096597a6cb47922017053f4437d2cb49a8d4
Wesley de Souza <wseysouza@gmail.com>
2022-04-01 17:36:01 -0300

    create pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.News</title>
+      </Head>
+      <h1>
+        Hello Word
+      </h1>
+    </>
+  )
+}
