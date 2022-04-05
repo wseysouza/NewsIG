679514ab128f9d17143845806bc09bcc2dd97488
Wesley de Souza <wseysouza@gmail.com>
2022-04-05 11:04:46 -0300

    change component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,15 @@
 import styles from './styles.module.scss';
 
+import Image from "next/image";
+
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Image src="/images/logo.svg" alt="ig.news" />
                 <nav>
                     <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <a>New Posts</a>
                 </nav>
             </div>
         </header>
